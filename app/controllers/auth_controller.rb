51bcbd60fc3f4c8fe89b77e530977e3947af1a2f
lawsonhung <lawsonhung@yahoo.com>
2020-05-18 17:33:44 -0400

    Defined auth#login

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,7 +1,17 @@
 class AuthController < ApplicationController
 
   def login
-    render json: 'hi'
+    # find a user
+    user = User.find_by(username: params[:username])
+    # if user exists, see if they really are the user via a password
+    is_authenticated = user.authenticate(params[:password])
+    # if all is well, send back the user
+    if is_authenticated
+      render json: user
+    else
+      render json: 'Youre not real... sorry'
+    end
+
   end
 
 end
