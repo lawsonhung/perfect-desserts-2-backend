cddfeb34841f09f98c95ee8becd72a2c770fea22
lawsonhung <lawsonhung@yahoo.com>
2020-05-19 17:01:25 -0400

    Local login successful

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,7 +3,7 @@ class AuthController < ApplicationController
   def login
     # find a user
     user = User.find_by(username: params[:username])
-    # if user exists, see if they really are the user via a password
+    # if user exists, see if they really are the user via a password 
     is_authenticated = user.authenticate(params[:password])
     # if all is well, send back the user
     if is_authenticated
